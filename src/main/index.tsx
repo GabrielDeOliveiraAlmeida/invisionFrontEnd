cf27da511d2629ca4e1d15d1d4a108cdbc56b49c
GabrielDeOliveiraAlmeida <gabriel.almeida@unesp.br>
2021-02-02 20:54:08 -0300

    chore: config do projeto e hello world

---
+++ src/main/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Login from '../presentations/pages/login/login';
+
+ReactDom.render(
+    <Login />,
+    document.getElementById('main')
+)
---
+++ src/presentations/pages/login/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Login: React.FC = () => {
+
+    return (
+        <div>Hello MUndo</div>
+    )
+}
+
+export default Login;
